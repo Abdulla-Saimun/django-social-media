19c8319d4b75c90dd9bac82f76bfbccee78b85f2
Abdulla-Saimun <abdullah35-1921@diu.edu.bd>
2022-06-05 14:28:17 +0600

    signin updated

--- core/views.py
+++ core/views.py
@@ -103,35 +103,37 @@ def settings(request):
 
 
 def signup(request):
-    if request.method == "POST":
+    
+    if request.method == 'POST':
         username = request.POST['username']
         email = request.POST['email']
-        pass1 = request.POST['password']
-        pass2 = request.POST['password2']
-        print(username, email, pass1, pass2)
-        if pass1 == pass2:
+        password = request.POST['password']
+        password2 = request.POST['password2']
+
+        if password == password2:
             if User.objects.filter(email=email).exists():
-                messages.info(request, 'this email taken')
+                messages.info(request, 'Email Taken')
                 return redirect('signup')
             elif User.objects.filter(username=username).exists():
-                messages.info(request, 'this username taken')
+                messages.info(request, 'Username Taken')
                 return redirect('signup')
             else:
-                user = User.objects.create(username=username, email=email, password=pass1)
+                user = User.objects.create_user(username=username, email=email, password=password)
                 user.save()
 
-                user_login = auth.authenticate(username=username, password=pass1)
+                #log user in and redirect to settings page
+                user_login = auth.authenticate(username=username, password=password)
                 auth.login(request, user_login)
 
-                # create a Profile object for the new user
+                #create a Profile object for the new user
                 user_model = User.objects.get(username=username)
                 new_profile = Profile.objects.create(user=user_model, id_user=user_model.id)
                 new_profile.save()
                 return redirect('settings')
         else:
-            messages.info(request, 'password not matches')
+            messages.info(request, 'Password Not Matching')
             return redirect('signup')
-
+        
     else:
         return render(request, 'signup.html')
 
