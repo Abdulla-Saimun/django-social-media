84c3401e010e2ee9be4eff84bf58fd5fb6b96c1f
Abdulla-Saimun <abdullah35-1921@diu.edu.bd>
2022-06-06 18:24:45 +0600

    suggestion added

--- core/views.py
+++ core/views.py
@@ -8,6 +8,7 @@ from django.contrib.auth.models import auth
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from itertools import chain
+import random
 
 
 # Create your views here.
@@ -29,7 +30,36 @@ def index(request):
         feed_lists = Post.objects.filter(user=usernames)
         feed.append(feed_lists)
     feed_list = list(chain(*feed))
-    context = {'user_profile': user_profile, 'posts': feed_list}
+
+    # user suggestion
+    all_users = User.objects.all()
+    user_following_all = list() # user follow them
+
+    for user in user_following:
+        user_list = User.objects.get(username = user.user)
+        user_following_all.append(user_list)
+    print(f'following {user_following_all}')
+
+    new_suggestions_list = [x for x in list(all_users) if (x not in list(user_following_all))] # people who are not followed by user
+    print(f'new suggestion list: {new_suggestions_list}')
+    current_user = User.objects.filter(username=request.user.username)
+    final_suggestions_list = [x for x in list(new_suggestions_list) if ( x not in list(current_user))]
+    random.shuffle(final_suggestions_list)
+    print(f'final suggestion list: {final_suggestions_list}')
+
+    username_profile = list()
+    username_profile_list = list()
+
+    for users in final_suggestions_list:
+        username_profile.append(users.id)
+
+    for ids in username_profile:
+        profile_lists = Profile.objects.filter(id_user=ids)
+        username_profile_list.append(profile_lists)
+    
+    print(f'username profile list f {username_profile_list}')
+    suggestions_username_profile_list = list(chain(*username_profile_list))
+    context = {'user_profile': user_profile, 'posts': feed_list, 'suggestions_username_profile_list':suggestions_username_profile_list[:4]}
     return render(request, 'index.html', context)
 
 @login_required(login_url='signin')
